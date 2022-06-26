1d1c497238b46579225ab76b0a43c357ea8bb224
iliescumichele <iliescumichele@gmail.com>
2022-06-27 00:08:10 +0200

    Viene aggiunto lo sconto all'user che è registrato

--- class/user.php
+++ class/user.php
@@ -5,15 +5,30 @@
         public $name;
         public $surname;
         public $email;
+        public $discount;
 
 
         //definisco un costruttore
-        public function __construct($_name, $_surname, $_email){
+        public function __construct($_name, $_surname, $_email, $_discount = 0){
             $this->name = $_name;
             $this->surname = $_surname;
             $this->email = $_email;
+            $this->discount = $_discount;
         }
     }
 
-    
+    class isUserRegistered extends User{
+        public $registered;
+        public $discount;
+      
+        function applyDiscount(){
+            if ($this->registered != null) {
+                $this->discount = "20%";
+            }
+        }
+
+        function setRegistered($_registered){
+            $this->registered = $_registered;
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -3,8 +3,17 @@
     include_once __DIR__ . "/class/item.php";
     include_once __DIR__ . "/class/user.php";
 
+    //istanziati oggetti 
+    $saccoCroccantini = new Item("Sacco di croccantini", 10);
+    $pallaTennis = new Item("Palla da tennis", 4);
+    $guinzaglio = new Item("Guinzaglio", 15);
 
-    $saccoCroccantini = new Products("Sacco di croccantini", 10);
-    $pallaTennis = new Products("Palla da tennis", 4);
-    $guinzaglio = new Products("guinzaglio", 15);
+
+    $Jack = new isUserRegistered("Jack", "Sparrow", "esempio@gmail.com", 0);
+
+    $Michele = new isUserRegistered("Michele", "Iliescu", "unalltroesempio@gmail.com", 0);
+    $Michele->setDiscount(true);
+
+
+
 ?>
